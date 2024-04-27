d637235f7b79b3122163c3849e4634b5e0ca2071
HIMU005 <hashanuzzaman99@gamil.com>
2024-04-27 08:44:28 +0600

    set up backend route for google sign user

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb://localhost:27017";
+// const uri = "mongodb://localhost:27017";
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6xa5uzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6xa5uzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,6 +30,29 @@ async function run() {
       const newUser = req.body;
       console.log(newUser);
       const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // app.get("users/");
+
+    app.put("/users", async (req, res) => {
+      const updatedUser = req.body;
+
+      filter = { email: updatedUser.email };
+      const options = { upsert: true };
+      const newUpdate = {
+        $set: {
+          name: updatedUser.displayName,
+          email: updatedUser.email,
+          photo: updatedUser.photoURL,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateOne, options);
       res.send(result);
     });
 
